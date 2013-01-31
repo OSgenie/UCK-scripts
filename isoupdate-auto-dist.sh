03d6efce9cfb184b3e9869e51f911b4e264da1f0
kirtley <kirtley@kirtley-zbox>
2013-01-31 02:21:32 -0800

    .

--- isoupdate-auto-dist.sh
+++ isoupdate-auto-dist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # kirtley wienbroer
 # kirtley@osgenie.com
-
+folderpath=/iso/downloads
 # updating server architecture
 server_arch=$(dpkg --print-architecture)
 server_release=$(lsb_release -rs)
 # customization scripts
-scriptpath=/home/kirtley/git/updater
+scriptpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 scripts=chroot
 
 function check_for_sudo ()
@@ -19,57 +19,64 @@ fi
 
 function update_valid_isos ()
 {
-folderpath=/iso/downloads
 array=$( ls $folderpath/ )
 for option in $array
 do
     iso_release='unknown'
     extension=${option##*.}
     name=$(basename $option .$extension)
-    arr=$(echo $name | tr "-" "\n")
-    for x in $arr
-    do
-        if [[ $x = 'i386' || $x = 'x86' || $x = 'i686' || $x = '32bit' || $x = '32' ]]; then
-            os_arch='i386'
-        elif [[ $x = 'amd64' || $x = 'x86_64' || $x = '64' || $x = '64bit' ]]; then
-            os_arch='amd64'
+    if [ $extension == "iso" ]; then
+        arr=$(echo $name | tr "-" "\n")
+        for x in $arr
+        do
+            if [[ $x = 'i386' || $x = 'x86' || $x = 'i686' || $x = '32bit' || $x = '32' ]]; then
+                os_arch='i386'
+            elif [[ $x = 'amd64' || $x = 'x86_64' || $x = '64' || $x = '64bit' ]]; then
+                os_arch='amd64'
+            fi
+            if [ ${x:0:5} == $server_release ]; then
+                iso_release='match'
+            fi
+        done
+        if [ $os_arch == $server_arch ] && [ $iso_release = 'match' ]; then
+            set_iso_arch
+            distro=$option
+            iso=$folderpath/$distro
+            unixtime=(`date +%s`)
+            update_iso
         fi
-        if [ ${x:0:5} == $server_release ]; then
-            iso_release='match'
-        fi
-    done
-if [ $os_arch == $server_arch ] && [ $iso_release = 'match' ]; then
-    set_iso_arch
-    distro=$option
-    iso=$folderpath/$distro
-    unixtime=(`date +%s`)
-    update_isos
-fi
+    fi
 done
 }
 
-function update_isos ()
+function update_iso ()
 {
 for type in live install
 do
     echo "+-------------------------------------------------------------------+"
     echo "+ UPDATING -- $distro for $type"
-    echo "+-------------------------------------------------------------------+"
+    echo "+-------------------------------------------------------------------+"    
     set_parameters
+# for testing purposes, this prevents a previously modified isos from being modified again
+#if [ ! -d $remasterdir ]; then
+    set_iso_arch
     unpack_iso
-    if [ $type == 'live' ]; then
-        set_isolinux_noprompt
-    fi
+    if [ -e $remasterdir/remaster-iso/casper/vmlinuz ];then
+        if [ $type == 'live' ]; then
+            set_isolinux_noprompt
+        fi
     modify_iso
     pack_iso
+    fi
+#fi
 done
 }
 
 function set_iso_arch ()
 {
-#Valid options are x86,x86_64,ia64,ppc
+# valid options are #x86,x86_64,ia64,ppc
 if [ $os_arch = "i386" ]; then
-isoarch=x86
+    isoarch=x86
 elif [ $os_arch = "amd64" ]; then
 isoarch=x86_64
 fi
@@ -81,20 +88,15 @@ fullname=$(basename "$iso")
 extension=${fullname##*.}
 directory=$(dirname $iso)
 name=$(basename $iso .$extension)
-if [ $extension == "iso" ]; then
-    remasterdir=/work/$type/$name
-    installdir=/iso/nfs/$type
-    isofilename="$installdir/$name-$unixtime.iso"
-    #	isoseed=/home/kirtley/Dropbox/Scripts/config_files/auto.seed
-    #	isocfg=/home/kirtley/Dropbox/Scripts/config_files/txt.cfg
-    else
-    exit
-fi
+remasterdir=/work/$type/$name
+installdir=/iso/nfs/$type
+isofilename="$installdir/$name-$unixtime.iso"
+#	isoseed=/home/kirtley/Dropbox/Scripts/config_files/auto.seed
+#	isocfg=/home/kirtley/Dropbox/Scripts/config_files/txt.cfg
 }
 
 function unpack_iso ()
 {
-# run ubuntu customization kit
 if [ ! -d $remasterdir ]; then
 	uck-remaster-unpack-iso $iso $remasterdir
 	uck-remaster-unpack-rootfs $remasterdir
@@ -115,13 +117,8 @@ chmod 444 $remasterdir/remaster-iso/isolinux/isolinux.cfg
 
 function modify_iso ()
 {
-if [ -e $remasterdir/remaster-iso/casper/vmlinuz ];then
-	cp -rpvf $scriptpath/$scripts $remasterdir/remaster-root/
-	uck-remaster-chroot-rootfs  $remasterdir /$scripts/customize-iso-$type-dist
-else
-	echo "not a live cd!"
-	exit
-fi
+cp -rpvf $scriptpath/$scripts $remasterdir/remaster-root/
+uck-remaster-chroot-rootfs  $remasterdir /$scripts/customize-iso-$type-dist
 }
 
 function pack_iso ()
