a9e4e993d93c99093d6bca42f5e274c5b3bb4ad3
kirtley <kirtley@kirtley-zbox>
2013-01-31 17:10:33 -0800

    .

--- isoupdate-auto-dist.sh
+++ isoupdate-auto-dist.sh
@@ -138,7 +138,7 @@ cp -v $remasterdir/remaster-new-files/$name-$unixtime.iso.md5 /iso/nfs/$type/md5
 
 # call functions
 check_for_sudo
-for i in {1..6}; do
+for i in {1..12}; do
 	echo $i
 	ls -1 /iso/downloads
 	sleep 10
