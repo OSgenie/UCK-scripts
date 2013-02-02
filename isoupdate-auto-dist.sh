72f85fec4f2598a8d24d310299b396d41d054c1b
kirtley <kirtley@kirtley-zbox>
2013-02-02 04:58:32 -0800

    .

--- isoupdate-auto-dist.sh
+++ isoupdate-auto-dist.sh
@@ -170,6 +170,7 @@ cp -v $remasterdir/remaster-new-files/$name-$unixtime.iso.md5 /iso/nfs/$type/md5
 
 # call functions
 check_for_sudo
-#sleep 120
+echo "Waiting 120 seconds for network resources"
+sleep 120
 create_array_of_valid_isos
 update_valid_isos
