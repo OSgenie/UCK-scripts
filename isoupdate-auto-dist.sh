59ada8a2eaf719a2529436d7306a33f53ba8ee27
kirtley <kirtley@kirtley-zbox>
2013-02-02 05:04:45 -0800

    .

--- isoupdate-auto-dist.sh
+++ isoupdate-auto-dist.sh
@@ -41,6 +41,7 @@ for option in $array; do
             list=(${list[@]} $option)
     fi
 done
+echo ${list[@]}
 }
 
 function update_valid_isos ()
@@ -48,6 +49,7 @@ function update_valid_isos ()
 for (( i=0;i<${#list[@]};i++)); do
     set_iso_arch
     distro=${list[$i]}
+    echo $distro
     iso=$folderpath/$distro
     unixtime=(`date +%s`)
     update_iso
@@ -170,7 +172,7 @@ cp -v $remasterdir/remaster-new-files/$name-$unixtime.iso.md5 /iso/nfs/$type/md5
 
 # call functions
 check_for_sudo
-echo "Waiting 120 seconds for network resources"
-sleep 120
+#echo "Waiting 120 seconds for network resources"
+#sleep 120
 create_array_of_valid_isos
 update_valid_isos
