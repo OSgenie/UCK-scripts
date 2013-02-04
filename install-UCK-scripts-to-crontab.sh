26b0e565b38153cd9bfb5590f241e9a5b2cef0eb
kirtley <kirtley@kirtley-zbox>
2013-02-04 12:03:03 -0800

    .

--- install-UCK-scripts-to-crontab.sh
+++ install-UCK-scripts-to-crontab.sh
@@ -11,7 +11,7 @@ fi
 
 function install_scripts_local_bin ()
 {
-install $scriptdir/isoupdate-auto-dist.sh /usr/local/bin/update-isos
+install $scriptdir/isoupdate-auto-list-dist.sh /usr/local/bin/update-isos
 install $scriptdir/isoupdate-bash.sh /usr/local/bin/isoupdate-bash
 mkdir /usr/local/bin/chroot
 install $scriptdir/chroot/* /usr/local/bin/chroot/
