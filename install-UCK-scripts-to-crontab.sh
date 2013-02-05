27828a701a67dc4b787e57fcf340d2c4818b66d9
kirtley <kirtley@kirtley-zbox>
2013-02-04 20:10:45 -0800

    .

--- install-UCK-scripts-to-crontab.sh
+++ install-UCK-scripts-to-crontab.sh
@@ -19,8 +19,9 @@ install $scriptdir/chroot/* /usr/local/bin/chroot/
 
 function configure_crontab ()
 {
+mkdir /var/log/update-isos
 echo "# m h  dom mon dow   command" | crontab -
-crontab -l | { cat; echo "@reboot /usr/local/bin/update-isos  > /var/log/update-isos/$(`date +%F`).log"; } | crontab -
+crontab -l | { cat; echo "@reboot /usr/local/bin/update-isos  > /var/log/update-isos/`date +%F`.log"; } | crontab -
 }
 
 check_for_sudo
