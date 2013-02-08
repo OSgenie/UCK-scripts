eb3eb0e8344c7fde064891413e69e1f8a1b8eb6e
kirtley <kirtley@kirtley-zbox>
2013-02-07 16:33:47 -0800

    .

---
+++ isoupdate-alternative.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+iso=
+echo "Needs to be written!!
+#NOTES:
+#sudo uck-remaster-unpack-iso $iso
+#sudo uck-remaster-prepare-alternate
+
+#copy the deb packages to the ~/tmp/remaster-iso/pool/extras
+
+#modify the preesed file"
