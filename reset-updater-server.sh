932aaca2d79ec41cc1416d9f43b6047b7a17c95e
kirtley <kirtley@kirtley-zbox>
2013-04-09 02:09:34 -0700

    Updater Server Reset Script

---
+++ reset-updater-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Deletes current iso working directory.
+sudo rm -r /work/live/*
+sudo rm -r /work/install/*
+# Tnstalls default settings.
+cd UCK-scripts/
+git pull
+sudo ./install-UCK-scripts-to-crontab.sh
